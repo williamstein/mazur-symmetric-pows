e53defb929703f2f7197c4c6f11b012030b903df
William Stein <wstein@gmail.com>
2012-12-15 20:15:26 -0500

    Add Barry's notes

---
+++ data.pyx
@@ -0,0 +1,4 @@
+#########################################################
+# Efficiently compute raw sum for a given elliptic curve
+# (c) William Stein
+#########################################################
