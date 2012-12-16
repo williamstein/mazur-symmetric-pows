6b4f8a9e90c1904ed2660ab9b385241ba80dec89
William Stein <wstein@gmail.com>
2012-12-15 21:03:26 -0500

    add data for 40a

--- data.pyx
+++ data.pyx
@@ -13,25 +13,35 @@ cdef double pi = math.pi
 
 def raw_data(E, B):
     """
-    Return the list of pairs (p, D_E(p)), for all primes p < B.
+    Return the list of pairs (p, D_E(p)), for all primes p < B, and
+    another list of pairs (p, running_average).
 
     E -- an elliptic curve over QQ
     B -- positive integer
     """
-    result = []
+    result = []; avgs = []
     aplist = E.aplist(B)
     primes = prime_range(B)
     assert len(aplist) == len(primes)
-    cdef double X
+    cdef double X, running_sum = 0, val = 0
     cdef int i, cnt = 0
     for i in range(len(aplist)):
         if aplist[i] < 0:
             cnt -= 1
         elif aplist[i] > 0:
             cnt += 1
-        X = primes[i]
-        result.append((primes[i], (log(X) / sqrt(X)) * cnt))
-    return result
+
+        if i == 0:
+            running_sum = 0
+        else:
+            running_sum += val*(primes[i] - primes[i-1] - 1)
+
+        X   = primes[i]
+        val = (log(X) / sqrt(X)) * cnt
+        running_sum += val
+        result.append((primes[i], val))
+        avgs.append((primes[i], running_sum/X))
+    return result, avgs
 
 def draw_plot(E, B, vanishing_symmetric_powers=None):
     if vanishing_symmetric_powers is None:
@@ -42,7 +52,11 @@ def draw_plot(E, B, vanishing_symmetric_powers=None):
         assert n%2 != 0
         k = (n-1)/2
         mean += (4/pi) * (-1)**(k+1)*(1/(2*k+1) + 1/(2*k+3))*order
-    d = raw_data(E, B)
+    d, running_average = raw_data(E, B)
     g = line(d)
     g += line([(0,mean), (d[-1][0],mean)], color='darkred')
+    g += line(running_average, color='green')
     return g
+
+
+# 2379b1  symmetric cube 4th order zero
